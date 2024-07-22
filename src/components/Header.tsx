694394981a340f345f3e1b30af2abd36b09f5dad
Cadence K <cadencehiggins@gmail.com>
2024-07-22 09:53:35 -0500

    base header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import HandWave from '../assets/HandWave.png'
+
+export default function Header() {
+
+    return (
+        <div className='flex items-center justify-between m-4 font-bold hover:underline text-lg'>
+            <img src={HandWave} alt="waving hand emoji" className='size-9 object-contain' />
+            <p>About Me</p>
+            <p>Contact</p>
+            <p>Projects</p>
+        </div>
+    )
+}
