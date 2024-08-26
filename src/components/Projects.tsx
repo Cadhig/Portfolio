c68dc1f38c28095d3b5337d4689bf28b8c54e2ef
Cadence K <cadencehiggins@gmail.com>
2024-08-26 17:35:31 -0500

    adjusted project modals for mobile view

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -34,22 +34,22 @@ function ProjectModal(props: ModalProps) {
             e.stopPropagation()
             props.setModal(false)
         }}>
-            <div className="bg-green-custom z-auto sticky rounded flex flex-col  items-center size-3/4 border border-grey-custom shadow-lg text-grey-custom defaultFont">
+            <div className="bg-green-custom py-4 z-auto gap-2 rounded flex flex-col overflow-y-auto overflow-x-hidden md:overflow-hidden items-center size-3/4 border border-grey-custom shadow-lg text-grey-custom defaultFont">
                 <div className="flex items-center">
                     <h1 className="text-4xl font-bold">{props.title}</h1>
                 </div>
-                <div className="flex size-full">
-                    <div className="flex items-center w-1/2 mx-4 justify-center">
-                        <img src={props.img2} alt={props.title} className="h-3/4 border border-grey-custom rounded" />
+                <div className="flex flex-col md:flex-row size-full">
+                    <div className="flex h-3/4 md:h-full justify-center md:items-center md:mx-2">
+                        <img src={props.img2} alt={props.title} className="rounded" />
                     </div>
-                    <div className="flex flex-col gap-4 items-center w-1/2 mx-4 justify-center">
-                        <div>
+                    <div className="flex flex-col gap-2 md:gap-4 w-full justify-center mx-4">
+                        <div className="flex flex-col items-center gap-4">
                             <h2 className="text-2xl text-center font-bold">Description</h2>
-                            <p>{props.description}</p>
+                            <p className="w-3/4 md:w-full">{props.description}</p>
                         </div>
-                        <div className="w-full">
+                        <div className="w-full flex flex-col items-center">
                             <h2 className="text-2xl text-center font-bold">Features</h2>
-                            <ul>
+                            <ul className="flex flex-col gap-2 w-3/4 md:w-full">
                                 <li>{props.feature1}</li>
                                 <li>{props.feature2}</li>
                                 <li>{props.feature3}</li>
@@ -57,23 +57,23 @@ function ProjectModal(props: ModalProps) {
                                 <li>{props.feature5}</li>
                             </ul>
                         </div>
-                        <div className="flex w-full gap-4">
-                            <p className="font-bold">Tech Used:</p>
-                            <div className="flex gap-4 items-center">
+                        <div className="flex w-full flex-col md:flex-row md:gap-4">
+                            <p className="font-bold text-center md:text-left">Tech Used:</p>
+                            <div className="grid grid-cols-2 gap-2 md:flex md:gap-4 md:items-center w-3/4 md:w-full">
                                 {props.tech.map((props: String, index: number) => {
                                     return <TechBlob text={props} key={index} />
                                 })}
                             </div>
                         </div>
-                        <div className="flex w-full gap-3">
-                            <p className="font-bold">Languages:</p>
-                            <div className="flex gap-4 items-center">
+                        <div className="flex w-full flex-col md:flex-row md:gap-4">
+                            <p className="font-bold text-center md:text-left">Languages:</p>
+                            <div className="grid grid-cols-2 gap-2 md:flex md:gap-4 md:items-center  w-3/4 md:w-full">
                                 {props.languages.map((props: String, index: number) => {
                                     return <LanguageBlob text={props} key={index} />
                                 })}
                             </div>
                         </div>
-                        <div className="flex gap-4 w-full">
+                        <div className="flex gap-4 w-3/4">
                             <div className='relative flex flex-col w-full md:w-1/2'>
                                 <div className="absolute inset-0 bg-grey-custom z-0 rounded" style={{ transform: 'translate(5px, 5px)' }}></div>
                                 <a href={props.git} target="_blank" className="relative bg-blue-custom rounded flex p-2 items-center gap-4 border border-grey-custom text-xl transition duration-150 ease-in-out hover:-translate-y-1"><Github />View Repo</a>
