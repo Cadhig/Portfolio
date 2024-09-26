1a6dec01a309fc09a3f464424c2de5b5076eee41
Cadence K <cadencehiggins@gmail.com>
2024-09-26 12:38:17 -0500

    changed font style

--- src/components/ProjectsMain.tsx
+++ src/components/ProjectsMain.tsx
@@ -4,7 +4,7 @@ import { Project, projects } from "../data/data"
 export default function ProjectsMain() {
 
     return (
-        <div className="flex flex-col justify-center items-center gap-4" id="projects">
+        <div className=" defaultFont flex flex-col justify-center items-center gap-4" id="projects">
             <h1 className="font-bold text-4xl text-center md:text-6xl md:w-1/2">Projects</h1>
             <div className="grid grid-rows-2 grid-cols-2 md:grid-cols-3 gap-4 m-4">
                 {projects.map((props: Project, index: number) => {
